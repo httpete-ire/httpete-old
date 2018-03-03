866f08323ab0bf4a347f592e81723b47f987b343
Pete <pete@Petes-MacBook-Pro.local>
2018-03-03 12:37:55 +0000

    Section responsive fixes

--- layout/Section.js
+++ layout/Section.js
@@ -1,7 +1,11 @@
 import glamorous from 'glamorous';
+import { mediaQueries } from './../config';
 
 const styles = {
   marginBottom: 100,
+  [mediaQueries.small]: {
+    marginBottom: '40px !important',
+  },
 };
 
 const Section = glamorous.section(styles);

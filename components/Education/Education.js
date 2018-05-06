afd086840d9e4926842a75d3387a1f821e7625ec
Pete Redmond <pete@httpete.com>
2018-05-06 20:11:01 +0100

    add space to education section on mobile

--- components/Education/Education.js
+++ components/Education/Education.js
@@ -1,21 +1,29 @@
-import glamorous from 'glamorous';
-import Dates from './../Dates/';
+import glamorous from "glamorous";
+import Dates from "./../Dates/";
+import { mediaQueries } from "./../../config";
 
 const Institute = glamorous.h5({
-  fontSize: '1.125em',
+  fontSize: "1.125em",
   fontWeight: 600,
   marginBottom: 5,
-  lineHeight: 1.5,
+  lineHeight: 1.5
 });
 
 const Course = glamorous.p({
-  lineHeight: 1.5,
+  lineHeight: 1.5
+});
+
+const Education = glamorous.div({
+  marginBottom: 0,
+  [mediaQueries.small]: {
+    marginBottom: 24
+  }
 });
 
 export default ({ institute, course, from, to }) => (
-  <div>
+  <Education>
     <Institute>{institute}</Institute>
     <Course>{course}</Course>
     <Dates from={from} to={to} />
-  </div>
+  </Education>
 );

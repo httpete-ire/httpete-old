a02b431bae388044bdde17a4b80cdb4a510f0574
Pete Redmond <pete@httpete.com>
2018-07-12 17:42:59 +0100

    add cache control to staic assets

--- server.js
+++ server.js
@@ -5,8 +5,23 @@ const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 const express = require('express');
+
 app.prepare().then(() => {
-  express()
-    .use(handler)
-    .listen(3000);
+  const server = express();
+
+  if (process.env.NODE_ENV === 'production') {
+    server.get(/^\/static\//, (_, res, nextHandler) => {
+      res.setHeader(
+        'Cache-Control',
+        'public, max-age=180, s-maxage=86400, immutable'
+      );
+      nextHandler();
+    });
+  }
+
+  server.use(handler).listen(3000);
 });
+
+// Cache-Control: max-age=180, s-maxage=86400
+
+// /static/

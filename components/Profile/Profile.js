416a484a3477e55c97ff7c753ab213b5fdd6a6d6
Pete Redmond <pete@httpete.com>
2018-09-08 22:52:19 +0100

    update Profile

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default () => (
   <p>
-    My name is Pete, I'm a Javascript engineer based in London. I have over five
-    years industry experience having worked for several start ups, a software
-    agency, IBM and Justgiving where I am currently employed.
+    Passionate and skilled front-end engineer with a strong background in
+    JavaScript. Over five years industry experience having worked for several
+    start ups, IBM and JustGiving where I am currently employed.
   </p>
 );

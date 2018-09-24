e10da8272cd681e25c22573a05db80f2cc375525
Pete Redmond <pete@httpete.com>
2018-09-24 22:18:40 +0100

    update profile

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -5,6 +5,6 @@ export default () => (
     I am a front-end engineer with a strong focus on JavaScript. I have over
     five years industry experience working across cross-functional teams on a
     wide range of projects. I am also a keen learner and always looking to
-    improve through side projects.
+    improve through side projects or open source work.
   </p>
 );
